97a4499893817c8b2b29f3c0be25d86e81d5540b
eldarlrd <eldarlrd@pm.me>
2023-12-07 15:11:24 +0400

    init: setup solid

---
+++ src/main.tsx
@@ -0,0 +1,28 @@
+/**
+ * @license AGPL-3.0-only
+ * TS Battleship - A Battleship game
+ * Copyright (C) 2023 Eldar Pashazade <eldarlrd@pm.me>
+ *
+ * This file is part of TS Battleship.
+ *
+ * TS Battleship is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * TS Battleship is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with TS Battleship. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/* @refresh reload */
+import { render } from 'solid-js/web'
+
+import { App } from './app.tsx'
+
+const root = document.getElementById('root')
+
+render(() => <App />, root!)
